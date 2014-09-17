7713851a211c6f3f69ac6a52cd3eccc6ca56bac5
Matheus Sampaio <matheus1401@gmail.com>
2014-09-17 09:28:14 -0300

    Change to Heroku API

---
+++ src/com/petremoto/asynctask/WaitForFeedTask.java
@@ -0,0 +1,75 @@
+
+package com.petremoto.asynctask;
+
+import android.content.Context;
+import android.os.AsyncTask;
+
+import com.petremoto.utils.MyLog;
+import com.petremoto.utils.PetRemotoUtils;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class WaitForFeedTask extends AsyncTask<String, Void, String> {
+    private final Context mContext;
+
+    public WaitForFeedTask(final Context context) {
+        mContext = context;
+    }
+
+    @Override
+    protected String doInBackground(final String... urls) {
+        // params comes from the execute() call: params[0] is the url.
+
+        int trys = 10;
+        String result = null;
+
+        while (trys > 0) {
+            trys--;
+
+            MyLog.info("GET: " + urls[0]);
+
+            result = PetRemotoUtils.downloadUrl(urls[0]);
+
+            if (result == null) {
+
+                try {
+                    Thread.sleep(1000);
+                } catch (final InterruptedException e) {
+                    MyLog.error(e.getMessage());
+                }
+            } else {
+                break;
+            }
+
+        }
+
+        return result;
+    }
+
+    // onPostExecute displays the results of the AsyncTask.
+    @Override
+    protected void onPostExecute(final String result) {
+        JSONObject mainObject;
+
+        try {
+            if (result != null) {
+                mainObject = new JSONObject(result);
+            } else {
+                mainObject = new JSONObject("{'status': 'failed'}");
+            }
+
+            MyLog.debug(mainObject.toString());
+            ((WaitForFeedInterface) mContext).callbackWaitForFeedJSON(mainObject);
+
+        } catch (final JSONException e) {
+            MyLog.debug(result);
+            MyLog.error(e.getMessage());
+
+        }
+    }
+
+    public interface WaitForFeedInterface {
+        public void callbackWaitForFeedJSON(final JSONObject json);
+    }
+}
